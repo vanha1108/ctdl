1f7624c7eaacca96d38a355eca013b50a6f14e9f
titaplamfbi <43457872+titaplamfbi@users.noreply.github.com>
2018-10-16 23:15:53 +0700

    Add files via upload

---
+++ giaodien/giaodien/Bill.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    public class Bill
+    {
+        private int status;
+        private DateTime? dateCheckOut;
+        private DateTime? dateCheckIn;
+        private int iD;
+
+        public DateTime? DateCheckIn
+        {
+            get
+            {
+                return dateCheckIn;
+            }
+
+            set
+            {
+                dateCheckIn = value;
+            }
+        }
+
+        public int ID
+        {
+            get
+            {
+                return iD;
+            }
+
+            set
+            {
+                iD = value;
+            }
+        }
+
+        public DateTime? DateCheckOut
+        {
+            get
+            {
+                return dateCheckOut;
+            }
+
+            set
+            {
+                dateCheckOut = value;
+            }
+        }
+
+        public int Status
+        {
+            get
+            {
+                return status;
+            }
+
+            set
+            {
+                status = value;
+            }
+        }
+        public Bill(int id,DateTime? dateCheckin, DateTime? dateCheckOut,int status)
+        {
+            this.ID = id;
+            this.DateCheckIn = dateCheckin;
+            this.DateCheckOut = dateCheckOut;
+            this.Status = status;
+        }
+        public Bill(DataRow row)
+        {
+            dateCheckin;
+            this.ID = (int)row["id"];
+            this.DateCheckIn = (DateTime?)row["dateCheckin"];
+            this.DateCheckOut = (DateTime?)row["dateCheckOut"];
+            this.Status = (int)row["status"];
+        }
+    }
+}
---
+++ giaodien/giaodien/BillInfo.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    public class BillInfo
+    {
+        private int iD;
+        private int billiD;
+        private int foodID;
+        private int Count;
+
+        public int ID
+        {
+            get
+            {
+                return iD;
+            }
+
+            set
+            {
+                iD = value;
+            }
+        }
+
+        public int BilliD
+        {
+            get
+            {
+                return billiD;
+            }
+
+            set
+            {
+                billiD = value;
+            }
+        }
+
+        public int FoodID
+        {
+            get
+            {
+                return foodID;
+            }
+
+            set
+            {
+                foodID = value;
+            }
+        }
+
+        public int Count1
+        {
+            get
+            {
+                return Count;
+            }
+
+            set
+            {
+                Count = value;
+            }
+        }
+        public BillInfo(int id, int billID, int foodID,int count)
+        {
+            this.ID = id;
+            this.BilliD = billID;
+            this.FoodID = foodID;
+            this.Count1 = count;
+        }
+    }
+}
